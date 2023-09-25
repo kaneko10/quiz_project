e6e4f22cf51932b0bfe9b241b80520b5c689c04a
{kaneko10} <{jinziyounan886@gmail.com}>
2023-09-26 00:02:37 +0900

    動画の順番をDBに保存

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Question, PlayTime, QuizAnswerTime, Questionnaire
+from .models import Question, PlayTime, QuizAnswerTime, Questionnaire, QuizOrder
 
 
 admin.site.register(Question)
 admin.site.register(PlayTime)
 admin.site.register(QuizAnswerTime)
 admin.site.register(Questionnaire)
+admin.site.register(QuizOrder)
