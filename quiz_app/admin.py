052f2e23608ad7bfd158b364cd9bcddb1301e1d4
{kaneko10} <{jinziyounan886@gmail.com}>
2023-09-25 19:58:55 +0900

    アンケート内容をデータベースに保存

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Question, PlayTime, QuizAnswerTime
+from .models import Question, PlayTime, QuizAnswerTime, Questionnaire
 
 
 admin.site.register(Question)
 admin.site.register(PlayTime)
 admin.site.register(QuizAnswerTime)
+admin.site.register(Questionnaire)
