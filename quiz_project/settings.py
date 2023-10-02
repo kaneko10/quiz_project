bf94cf21608abbcf2334191cd9adf2989dd78db0
imanishi03 <imasa@toralab.org>
2023-10-02 13:45:22 +0900

    ALLOWED_HOSTS変更

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -30,7 +30,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'quiz.onrender.com']
+# ALLOWED_HOSTS = ['127.0.0.1', 'quiz.onrender.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 
